f7f16302fe7d5ae8ebc81cc7f40f7778088dfd41
Jack Meng <error666jm@gmail.com>
2021-10-20 21:00:05 -0400

    reinit

---
+++ self_test/pwner/pwner.c
@@ -0,0 +1,43 @@
+// user modifications
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/*
+void flag()
+{
+    char buf[80];
+    FILE *f = fopen("./flag.txt", "r");
+    if (f == NULL)
+    {
+        printf("Flag File is Missing. please contact an Admin if you are running this on the shell server.\n");
+        exit(0);
+    }
+
+    fflush(stdout);
+    fgets(buf, 80, f);
+    puts(buf);
+}
+*/
+
+int main(void)
+{
+    char password[33];
+    while (1)
+    {
+        puts("input\n");
+        fgets(password, 33, stdin);
+        if (strcmp(password, "\x45\x49\x04\x84\x05\x84\x23\x02\x99\x32\x03\n") == 0)
+        {
+            puts("Correct");
+        }
+        else
+            puts("Nope!");
+    }
+
+    exit(0);
+    return 0;
+}
