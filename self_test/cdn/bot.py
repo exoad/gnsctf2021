506d4095c4d295657bb908b0daf3dd253403c532
Jack Meng <error666jm@gmail.com>
2021-10-24 21:07:58 -0400

    cached

--- self_test/cdn/bot.py
+++ self_test/cdn/bot.py
@@ -1,14 +1,11 @@
 import pyppeteer
 
-with open('flag.txt', 'r') as f:
-    flag = f.read()
-
 
 async def visit(url):
-    url = 'http://0.0.0.0:3010/static/%s.html' % url
+    url = 'http://localhost:3010/static/%s.html' % url
     browser = await pyppeteer.launch(handleSIGINT=False,
                                      handleSIGTERM=False,
                                      handleSIGHUP=False)
     page = await browser.newPage()
-    await page.setCookie({'url': url, 'name': 'flag', 'value': flag})
+    await page.setCookie({'url': url, 'name': 'flag', 'value': "flag"})
     await page.goto(url)
